ee10d5c2f4c8f0014d49795aae17c6403012ed19
Pudding <s.kokuryo@gmail.com>
2019-06-22 14:09:13 +0900

    Add verbose flag

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 )
 
 var flgVersion bool
+var flgVerbose bool
 
 func main() {
 	rootCmd := flag.NewFlagSet("Root", flag.ContinueOnError)
 	rootCmd.BoolVar(&flgVersion, "v", false, "Print version")
 	rootCmd.BoolVar(&flgVersion, "version", false, "Print version")
+	rootCmd.BoolVar(&flgVerbose, "verbose", false, "Output log")
+
 	if err := rootCmd.Parse(os.Args[1:]); err != nil {
 		if err == flag.ErrHelp {
 			os.Exit(0)
