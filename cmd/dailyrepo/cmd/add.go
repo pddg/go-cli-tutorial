01e58678c23b4e689074a2fac2c0d6d842283be7
Pudding <s.kokuryo@gmail.com>
2019-06-22 16:51:12 +0900

    Implement report creation from template

--- cmd/dailyrepo/cmd/add.go
+++ cmd/dailyrepo/cmd/add.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
-	"fmt"
+	"io/ioutil"
+	"os"
+	"text/template"
 	"time"
 
+	_ "github.com/pddg/go-cli-tutorial/statik"
+	"github.com/rakyll/statik/fs"
 	"github.com/spf13/cobra"
 )
 
@@ -24,7 +28,21 @@ func init() {
 	addCmd.Flags().StringP("name", "n", time.Now().Format("2006-01-02")+".md", "filename")
 }
 
-func generateReport(filename string) error {
-	fmt.Println(filename)
+func generateReport(fileName string) error {
+	statikFs, _ := fs.New()
+	// template読み込む
+	tplFile, _ := statikFs.Open("/report.md.tmpl")
+	byteTmpl, _ := ioutil.ReadAll(tplFile)
+	stringTmpl := string(byteTmpl)
+	tmpl := template.Must(template.New("report").Parse(stringTmpl))
+	// Todayを差し込む
+	reportFile, _ := os.Create(fileName)
+	reportMeta := struct {
+		Today string
+	}{
+		Today: time.Now().Format("2006-01-02"),
+	}
+	// text/templateとhtml/templateで挙動が違うので注意
+	_ = tmpl.Execute(reportFile, reportMeta)
 	return nil
 }
