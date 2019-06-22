643f44eb03849ea7bc296a599a53026aae317c51
Pudding <s.kokuryo@gmail.com>
2019-06-22 16:37:10 +0900

    Add filename option

--- cmd/dailyrepo/cmd/add.go
+++ cmd/dailyrepo/cmd/add.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -10,11 +11,20 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "今日の日報を追加",
 	Long:  "テンプレートを元に今日の日報の雛形を作成する",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fileName, _ := cmd.Flags().GetString("name")
+		_ = generateReport(fileName)
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(addCmd)
+
+	addCmd.Flags().StringP("name", "n", time.Now().Format("2006-01-02")+".md", "filename")
+}
+
+func generateReport(filename string) error {
+	fmt.Println(filename)
+	return nil
 }
